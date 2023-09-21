6668206e284431b592df8a9a43d19f46e76b1ada
kolhetejas <119657723+kolhetejas@users.noreply.github.com>
2023-09-21 14:26:34 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// -------------------typing animations --------------
+var typed =new Typed(".typing",{
+    strings:["Web Designer" , "Frontend Developer","Sportsman Person"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
